024af0811228aa88da83f10893e65c7c235925ac
MarshmallowCafe <ezutp@yahoo.com>
2018-02-07 10:31:45 -0800

    allows arbitry public playlist given as input

--- getPlaylistNames.py
+++ getPlaylistNames.py
@@ -4,10 +4,9 @@ from html.parser import HTMLParser
 import re
 
 
-#playlistUrl = input("gib nem: ")
+playlist_url = input("gib nem: ")
 
-with urllib.request.urlopen(
-        'https://www.youtube.com/playlist?list=PLp2ovQedKSi5VjHDms1hIYqoUByLaBkgK') as response:
+with urllib.request.urlopen(playlist_url) as response:
   playlist = response.read()
   soup = BS(playlist, "lxml")
   
@@ -19,22 +18,3 @@ titles_str = '\n'.join(titles).encode('cp1252','replace').decode('cp1252')
 print(titles_str)
 with open("playListNames.txt", "a") as f:
     f.write(titles_str)
-
-
-#
-#titles = ""
-#class MyHTMLParser(HTMLParser):
-#    def handle_starttag(self, tag, attrs):
-#        for attr in attrs:
-#            if attr[0] == "data-title":
-#                global titles
-#                titles += attr[1] + "\n"
-#        
-#parser = MyHTMLParser()
-#parser.feed(html)
-#print(titles)
-#
-#with open("playListNames2.txt", "a") as f:
-#    f.write(titles)
-#
-#print("yay")
