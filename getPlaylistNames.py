4f36c3f73443421d8434a33a3bfd538ef9505b00
MarshmallowCafe <ezutp@yahoo.com>
2018-02-08 11:24:07 -0800

    updates: different playlists are saved to different files; can now save Chinese characters

--- getPlaylistNames.py
+++ getPlaylistNames.py
@@ -1,20 +1,24 @@
 from bs4 import BeautifulSoup as BS
 import urllib.request
-from html.parser import HTMLParser
 import re
 
-
+''' TODO
+- append new titles to playlist textfile outputs without overwritting original
+- allow user authentification for private playlists
+'''
 playlist_url = input("gib nem: ")
 
 with urllib.request.urlopen(playlist_url) as response:
-  playlist = response.read()
+  playlist = response.read()#.decode('utf-8')
   soup = BS(playlist, "lxml")
+  
+playlist_name = soup.find('title').contents[0]
   
 title_attrs = soup.find_all(attrs={"data-title":re.compile(r".*")})
 titles = [tag["data-title"] for tag in title_attrs]
 
-titles_str = '\n'.join(titles).encode('cp1252','replace').decode('cp1252')
+titles_str = '\n'.join(titles)
 
 print(titles_str)
-with open("playListNames.txt", "a") as f:
+with open(playlist_name + ".txt", "a", encoding="utf-8") as f:
     f.write(titles_str)
